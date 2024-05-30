59d7a0bf47ce662c79610b352b258159ad2d2109
Charlotte Roy <roychrltt@chacha.home>
2024-05-30 09:52:42 +0200

    yeah

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: xiaxu <xiaxu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 13:24:29 by xiaxu             #+#    #+#             */
-/*   Updated: 2024/05/30 08:53:45 by xiaxu            ###   ########.fr       */
+/*   Updated: 2024/05/30 09:51:58 by xiaxu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,12 +36,6 @@ int	print_s(const char *s)
 	return (i);
 }
 
-int	print_percent(void)
-{
-	write (1, "%", 1);
-	return (1);
-}
-
 int	ft_format(const char c, va_list args)
 {
 	int	print_len;
@@ -59,8 +53,8 @@ int	ft_format(const char c, va_list args)
 		print_len += print_u(va_arg(args, unsigned int));
 	else if (c == 'x' || c == 'X')
 		print_len += print_x(va_arg(args, unsigned int), c);
-	else if (c == '%')
-		print_len += print_percent();
+	else
+		print_len += print_c(c);
 	return (print_len);
 }
 
@@ -78,6 +72,10 @@ int	ft_printf(const char *str, ...)
 		if (str[i] == '%' && str[i + 1])
 		{
 			print_len += ft_format(str[i + 1], args);
+			if (str[i + 1] == 'c' || str[i + 1] == 's' || str[i + 1] == 'd'
+				|| str[i + 1] == 'i' || str[i + 1] == 'u' || str[i + 1] == 'p'
+				|| str[i + 1] == 'x' || str[i + 1] == 'X')
+				va_arg(args, int);
 			i++;
 		}
 		else
@@ -87,12 +85,3 @@ int	ft_printf(const char *str, ...)
 	va_end(args);
 	return (print_len);
 }
-/*
-int main()
-{
-	int i = ft_printf(" %c %c %c \n", '0', 0, '1');
-	ft_printf("%d\n", i);
-	int j = printf(" %c %c %c \n", '0', 0, '1');
-	printf("%d\n", j);
-}
-*/
